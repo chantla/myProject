ef11616764f048166701fe751b86a814c78fd89e
ycnj <ycnj32982@naver.com>
2018-11-06 14:42:12 +0900

    헬로 삭제

--- BatSearch/src/test/hello/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package test.hello;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Time is 1:45 PM");
-		System.out.println("아름다운 밤이야아아");
-	}
-}
